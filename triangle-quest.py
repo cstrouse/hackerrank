8900b4bdecfc67c50973854dd4fbbe4c22aeba23
Harsh Oswal <69387265+harshoswal711@users.noreply.github.com>
2020-12-22 01:39:33 +0530

    Update triangle-quest.py
    Please update your solution it is not working

--- triangle-quest.py
+++ triangle-quest.py
@@ -3,5 +3,8 @@
 # This article really helps make sense of repdigits and how to figure them out
 #   http://www.fact-index.com/r/re/repunit.html
 
-for i in range(1, input()):
-    print ((10 ** i - 1) / 9) * i
+for i in range(1, int(input())):
+    print(int(i * 10**i / 9))
+    
+    
+#Contributed By Mr Cool
